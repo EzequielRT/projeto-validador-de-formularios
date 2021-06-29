c261a0a4fd28072b11045ffc2095f30ff90406df
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-28 23:31:07 -0300

    Tratando os inputs e exibindo erros

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ let subValidador = {
 
         let inputs = form.querySelectorAll('input');
 
+        subValidador.clearErrors();
+
         for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
             let check = subValidador.checkInput(input);
             if(check != true) {
                 send = false;
-                console.log(check);
+                subValidador.showError(input, check);
             }
         }
 
@@ -39,6 +41,26 @@ let subValidador = {
         }
 
         return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = 'red';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i = 0; i<errorElements.length; i++) {
+            errorElements[i].remove();
+        }
     }
 };
 
